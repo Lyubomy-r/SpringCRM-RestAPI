1b6785d94d6b9279fe513ebc27bf3db87107fbc4
Lyubomyr <bobboby403@gmail.com>
2023-02-04 22:31:11 +0200

    First commit in project

---
+++ src/main/java/SpringCrmRestAPI/dao/CustomerDAO.java
@@ -0,0 +1,17 @@
+package SpringCrmRestAPI.dao;
+
+import java.util.List;
+
+import SpringCrmRestAPI.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+	
+}
---
+++ src/main/java/SpringCrmRestAPI/rest/CustomerRestController.java
@@ -0,0 +1,27 @@
+package SpringCrmRestAPI.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import SpringCrmRestAPI.entity.Customer;
+import SpringCrmRestAPI.service.CustomerService;
+
+@RestController
+@RequestMapping("/api")
+public class CustomerRestController {
+	
+	@Autowired
+	private CustomerService customerService ;
+	
+	@GetMapping("/customers")
+	public List<Customer> getCustomers(){
+		
+		return customerService.getCustomers();
+		
+	}
+
+}
